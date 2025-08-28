0f0765315263ba125c3ce684fd58aec8c0d77758
Sonya <sonyacqu@gmail.com>
2025-08-28 15:54:21 -0400

    feat: color change when mousedown and mouseenter

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const container = document.querySelector("#container");
+let mousedown = false;
+
 
 function createFirstGrids(){
   for (let i = 1; i <= 16; i++){ //creates number of vertical grids
@@ -15,8 +17,17 @@ function createSecondGrids(columnDiv){
     columnDiv.appendChild(insideDiv);
     insideDiv.classList.add("insideDiv");
 
-    insideDiv.addEventListener("click", () =>{
+    insideDiv.addEventListener("mousedown", () => {
+      mousedown = true;
       insideDiv.classList.add("clicked");
+    })
+    insideDiv.addEventListener("mouseup", () => {
+      mousedown = false;
+    })
+    insideDiv.addEventListener("mouseenter", () => {
+      if (mousedown === true){
+        insideDiv.classList.add("clicked");
+      }
     })
   }
 }
